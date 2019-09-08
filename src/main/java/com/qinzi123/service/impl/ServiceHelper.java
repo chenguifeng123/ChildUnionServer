21d60f38cfded6c362cdaa15ffecc7d83f2b7daf
chenguifeng <22255946@qq.com>
2019-09-08 19:04:34 +0800

    modify send template title

--- src/main/java/com/qinzi123/service/impl/ServiceHelper.java
+++ src/main/java/com/qinzi123/service/impl/ServiceHelper.java
@@ -19,7 +19,8 @@ public class ServiceHelper {
 		cardMessage.setGiveLike(Integer.parseInt(map.get("give_like").toString()));
 		cardMessage.setReadCount(Integer.parseInt(map.get("read_count").toString()));
 		//设置通知消息标题
-		cardMessage.setTitle(message);
+		//cardMessage.setTitle(message);
+		cardMessage.setTitle(map.get("title").toString());
 		cardMessage.setMessage(map.get("message").toString());
 
 		return cardMessage;
@@ -31,11 +32,13 @@ public class ServiceHelper {
 		cardMessageReply.setCardId(Integer.parseInt(map.get("card_id").toString()));
 		cardMessageReply.setMessageId(Integer.parseInt(map.get("message_id").toString()));
 		//设置通知消息标题
-		cardMessageReply.setTitle(message);
+		//cardMessageReply.setTitle(message);
+		cardMessageReply.setTitle(map.get("reply_message").toString());
 		//cardMessageReply.setReplyId(Integer.parseInt(map.get("reply_id").toString()));
 		// 固定通知作者
 		cardMessageReply.setReplyId(0);
 		cardMessageReply.setReplyMessage(map.get("reply_message").toString());
+
 		return cardMessageReply;
 	}
 }
